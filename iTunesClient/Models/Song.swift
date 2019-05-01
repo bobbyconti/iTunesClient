34722066dd05e78c543a610eaaac21e88df8d5c7
Bobby Conti <No Email>
2019-05-01 13:13:12 -0400

    Added Song model

---
+++ iTunesClient/Models/Song.swift
@@ -0,0 +1,18 @@
+//
+//  Song.swift
+//  iTunesClient
+//
+//  Created by Bobby Conti on 5/1/19.
+//  Copyright © 2019 Bobby Conti. All rights reserved.
+//
+
+import Foundation
+
+struct Song {
+    let id: Int
+    let name: String
+    let censoredName: String
+    let trackTime: Int
+    let isExplicit: Bool
+}
+
