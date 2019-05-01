bed70f085fbba56f75e0ecbc547b259dda4cc69b
Bobby Conti <No Email>
2019-05-01 13:45:54 -0400

    Added SearchResultsController, search bar and cancel button

---
+++ iTunesClient/Controllers/SearchResultsController.swift
@@ -0,0 +1,34 @@
+//
+//  SearchResultsController.swift
+//  iTunesClient
+//
+//  Created by Bobby Conti on 5/1/19.
+//  Copyright © 2019 Bobby Conti. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsController: UITableViewController {
+    
+    let searchController = UISearchController(searchResultsController: nil)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(SearchResultsController.dismissSearchResultsController))
+        
+        tableView.tableHeaderView = searchController.searchBar
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchResultsUpdater = self
+    }
+    
+    @objc func dismissSearchResultsController() {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension SearchResultsController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        print(searchController.searchBar.text!)
+    }
+}
