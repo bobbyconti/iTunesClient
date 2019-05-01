f305109e9e3066a1953f046facdb156dc7bc66a8
Bobby Conti <No Email>
2019-05-01 15:01:47 -0400

    Connected SearchResultController to AlbumListController showing list of albums for selected artist

--- iTunesClient/Data Sources/SearchResultsDataSource.swift
+++ iTunesClient/Data Sources/SearchResultsDataSource.swift
@@ -38,4 +38,10 @@ class SearchResultsDataSource: NSObject, UITableViewDataSource {
         
         return cell
     }
+    
+    // MARK: - Helper Methods
+    
+    func artist(at indexPath: IndexPath) -> Artist {
+        return data[indexPath.row]
+    }
 }
