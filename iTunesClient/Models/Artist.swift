8d587a28b56781fb232a6a718b7df4ef2b9bd35b
Bobby Conti <No Email>
2019-05-01 13:12:41 -0400

    Added Artist model

---
+++ iTunesClient/Models/Artist.swift
@@ -0,0 +1,23 @@
+//
+//  Artist.swift
+//  iTunesClient
+//
+//  Created by Bobby Conti on 5/1/19.
+//  Copyright © 2019 Bobby Conti. All rights reserved.
+//
+
+import Foundation
+
+class Artist {
+    let id: Int
+    let name: String
+    let primaryGenre: Genre
+    var albums: [Album]
+    
+    init(id: Int, name: String, primaryGenre: Genre, albums: [Album]) {
+        self.id = id
+        self.name = name
+        self.primaryGenre = primaryGenre
+        self.albums = albums
+    }
+}
