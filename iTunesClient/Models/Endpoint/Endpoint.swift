f8f8822ace1598d93ecc36bc2ca57e61c63b7776
Bobby Conti <No Email>
2019-05-07 18:33:23 -0400

    Added ItunesMedia and Endpoint to model search terms and media type

---
+++ iTunesClient/Models/Endpoint/Endpoint.swift
@@ -0,0 +1,63 @@
+//
+//  Endpoint.swift
+//  iTunesClient
+//
+//  Created by Bobby Conti on 5/1/19.
+//  Copyright © 2019 Bobby Conti. All rights reserved.
+//
+
+import Foundation
+
+protocol Endpoint {
+    var base: String { get }
+    var path: String { get }
+    var queryItems: [URLQueryItem] { get }
+}
+
+extension Endpoint {
+    var urlComponents: URLComponents {
+        var components = URLComponents(string: base)!
+        components.path = path
+        components.queryItems = queryItems
+        
+        return components
+    }
+    
+    var request: URLRequest {
+        let url = urlComponents.url!
+        return URLRequest(url: url)
+    }
+}
+
+enum Itunes {
+    case search(term: String, media: ItunesMedia?)
+}
+
+extension Itunes: Endpoint {
+    var base: String {
+        return "https://itunes.apple.com"
+    }
+    
+    var path: String {
+        switch self {
+        case .search: return "/search"
+        }
+    }
+    
+    var queryItems: [URLQueryItem] {
+        switch self {
+        case .search(let term, let media):
+            var result = [URLQueryItem]()
+            
+            let searchTermItem = URLQueryItem(name: "term", value: term)
+            result.append(searchTermItem)
+            
+            if let media = media {
+                let mediaItem = URLQueryItem(name: "media", value: media.description)
+                result.append(mediaItem)
+            }
+            
+            return result
+        }
+    }
+}
