a8bb27c4667fdff9ea2afff4b42150018519d556
Bobby Conti <noname@email.com>
2019-06-29 11:44:19 -0400

    Added ArtworkDownloader operation logic and modified Album model

---
+++ iTunesClient/Networking/ArtworkDownloader.swift
@@ -0,0 +1,42 @@
+//
+//  ArtworkDownloader.swift
+//  iTunesClient
+//
+//  Created by Bobby Conti on 6/29/19.
+//  Copyright © 2019 Bobby Conti. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ArtworkDownloader: Operation {
+    let album: Album
+    
+    init(album: Album) {
+        self.album = album
+        super.init()
+    }
+    
+    override func main() {
+        if self.isCancelled {
+            return
+        }
+        
+        guard let url = URL(string: album.artworkUrl) else {
+            return
+        }
+        
+        let imageData = try! Data(contentsOf: url)
+        
+        if self.isCancelled {
+            return
+        }
+        
+        if imageData.count > 0 {
+            album.artwork = UIImage(data: imageData)
+            album.artworkState = .downloaded
+        } else {
+            album.artworkState = .failed
+        }
+    }
+}
